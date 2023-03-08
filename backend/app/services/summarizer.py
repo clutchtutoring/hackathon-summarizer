46cebd85c169f0ed6db89ccf2ab244ab7dddf515
Augusto Zanoni <augustorz@tutanota.com>
2023-03-08 16:09:38 -0300

    fix: summarize service

--- backend/app/services/summarizer.py
+++ backend/app/services/summarizer.py
@@ -4,14 +4,14 @@ import ffmpeg
 import whisper
 
 # Extract Video Audio to mp3
-stream = ffmpeg.input("./videos/input.mp4")
-stream = ffmpeg.output(stream, "./audios/output.mp3")
+stream = ffmpeg.input("../assets/videos/input.mp4")
+stream = ffmpeg.output(stream, "../assets/audios/output.mp3")
 ffmpeg.overwrite_output(stream)
 ffmpeg.run(stream)
 
 # Transcribe audio to text
 model = whisper.load_model("base")
-result = model.transcribe("./audios/output.mp3")
+result = model.transcribe("../assets/audios/output.mp3")
 
 # Summarize text
 docs = [Document(result["text"])]
