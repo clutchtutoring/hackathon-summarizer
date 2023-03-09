e779d8a8f98f1044e12a7706ecf383843ab001da
Augusto Zanoni <augustorz@tutanota.com>
2023-03-08 21:33:10 -0300

    fix: adds comment to function

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,6 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
+    """Settings for the app."""
     app_name: str = "Summarizer API"
     redis_url: str = "redis://localhost:6379"
