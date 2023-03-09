23c53f0890f6711b415900057c9db8235d5252b0
Onnassiz <onnassiz@gmail.com>
2023-03-09 06:32:00 +0100

    fix ssl error

--- download_models.py
+++ download_models.py
@@ -1,5 +1,9 @@
+import ssl
+
 import whisper
 from haystack.nodes import TransformersSummarizer
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 whisper.load_model("base")
 TransformersSummarizer(model_name_or_path="philschmid/bart-large-cnn-samsum")
